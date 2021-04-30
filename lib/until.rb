df25f318a7ac5dde8561efd7622566d8ccd47f7a
captivating-script-3337 <rchow3illinois@gmail.com>
2021-04-30 22:08:12 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,9 @@
 def using_until
   levitation_force = 6
-  if levitation_force < 10,
-    puts "Wingardium Leviosa"
-  #your code here
 
 end
+  until levitation_force == 10
+    levitation_force += 1
+    puts "Wingardium Leviosa"
+  end 
+end 
